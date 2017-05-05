1f974adedc263c91ed03df42388b983eab887301
SimonQJL <a5620165@vip.qq.com>
2017-05-05 16:45:54 +0800

    Update map.js

--- map.js
+++ map.js
@@ -16,17 +16,17 @@ $(function () {
     testMap = map;
     //由于地图数据使用了异步加载，为避免出错请把所有的逻辑放在 mapready 事件内
     map.on("mapready", function (e) {
-        map.on("click", function (e) {
-            var poi = map.getShop(e.mapPoint);
-            if (poi.name) {
-                map.infoPopup({
-                    minWidth: 100,
-                    maxWidth: 500
-                }).setContent(poi.name).setPoint(e.mapPoint);
-            }
-        });
+        // map.on("click", function (e) {
+            // var poi = map.getShop(e.mapPoint);
+            // if (poi.name) {
+                // map.infoPopup({
+                    // minWidth: 100,
+                    // maxWidth: 500
+                // }).setContent(poi.name).setPoint(e.mapPoint);
+            // }
+        // });
 
-        setTimeout(getPoint, 1000);//两秒刷新一次坐标
+        setTimeout(getPoint, 1000);
 
         function getPoint() {
             $.ajax({
@@ -45,9 +45,9 @@ $(function () {
                     else if (Point_Img.length > obj.length) {
                         removePoint(obj);//移除點座標					  
                     }
-                    setTimeout(getPoint, 500);
+                    setTimeout(getPoint,500);//0.5刷新一次坐标
                 }, error: function () {
-                    alert("智石返回空數據，無法繪點");
+                    alert("返回空數據，無法繪點");
                 }
             });
         }
@@ -68,6 +68,7 @@ $(function () {
                     }
                 }
                 if (b) {
+					
                     var image = new BRTSymbol.Icon([point[i].x, point[i].y], {
                         url: '../../Img/me_point.png',
                         width: 20,
@@ -76,7 +77,11 @@ $(function () {
                     }).addTo(map); //添加圖片
                     Point_Img.push(image);//將對象加入Point_Img數組
                     Point_Img[Point_Img.length - 1].id = point[i].ble_addr;
-                    var myLabel = new BRTSymbol.divIcon([point[i].x, point[i].y], {
+					 //获取当前房间名字
+                     var xy = { "x": Point_Img[Point_Img.length - 1]._point.x, "y": Point_Img[Point_Img.length - 1]._point.y };
+                     var shop = map.getShop(xy);
+					 $(Point_Img[Point_Img.length - 1].domNode).on('click',{coustomername:Point_Img[Point_Img.length - 1].id,roomname:shop.name},getroomname);//循環綁定click事件，					 
+                     var myLabel = new BRTSymbol.divIcon([point[i].x, point[i].y], {
                         html: point[i].ble_addr,
                         className: 'User',
                         fonsSize: 12,
@@ -84,21 +89,23 @@ $(function () {
                         color: "#FF3300",
                     }).addTo(map);	//添加文字
                     Point_Lable.push(myLabel);//將對象加入Point_Img數組			
-                    //获取当前房间名字
-                    // var xy = { "x": point.x, "y": point.y };
-                    // var shop = map.getShop(xy);
-                    // console.log(shop.name);
+                   
                 }
 
             }
-        }
+			
+		}
 
 
         function reloadPoint(point) {
             for (var i in Point_Img) {
                 if (Point_Img[i].id == point[i].ble_addr) {
-                    Point_Img[i].setPoint([point[i].x, point[i].y]);
+                    Point_Img[i].setPoint([point[i].x, point[i].y]);//重設座標
                     Point_Lable[i].setPoint([point[i].x, point[i].y]);
+					var xy = { "x": point[i].x,"y":point[i].y };
+                    var shop = map.getShop(xy);
+					$(Point_Img[i].domNode).off('click');//解除舊的绑定事件
+					$(Point_Img[i].domNode).on('click',{coustomername:Point_Img[i].id,roomname:shop.name},getroomname);//循環綁定click事件，	
                 }
             }
 
@@ -114,6 +121,8 @@ $(function () {
                     }
                 }
                 if (b) {
+					$(Point_Img[i].domNode).remove();
+				    $(Point_Lable[i].domNode).remove()
                     Point_Img.splice(i, 1);
                     Point_Lable.splice(i, 1);
                 }
@@ -122,8 +131,12 @@ $(function () {
         }
 
     });
+	
+	  function getroomname(event){
+			
+			console.log('客戶:'+event.data.coustomername+','+'房间:'+event.data.roomname);
+			// $('#CustomerInfomation_Container').html('客戶:'+event.data.coustomername+','+'房间:'+event.data.roomname);
+			// $('#CustomerInfomation_Container').show();	
+        }
 
 });
-
-
-
